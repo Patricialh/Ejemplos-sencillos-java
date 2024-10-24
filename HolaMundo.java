6b56684ffa09fad0b7b6e81df383879609941e8f
Patricia L <Lh.Patricia90@gmail.com>
2024-10-24 20:08:35 +0200

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// ESTO ES PARA ESCRIBIR COMENTARIOS
+		//Aquí empieza mi programa
+		
+		System.out.println ("Hola Mundo");
+		//esta tarea esdta sin terminar
+	}
+
+}
